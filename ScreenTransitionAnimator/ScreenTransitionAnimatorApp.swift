0c105b33d13ea1a63450c5650b6be9df093e2a91
Vinay <vin070@gmail.com>
2025-12-09 12:00:32 +0530

    Add menu bar app delegate and main app structure

---
+++ ScreenTransitionAnimator/ScreenTransitionAnimatorApp.swift
@@ -0,0 +1,53 @@
+import AppKit
+import SwiftUI
+
+@main
+struct ScreenTransitionAnimatorApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        Settings {
+            EmptyView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+    var popover: NSPopover?
+    var screenMonitor = ScreenMonitor()
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Create menu bar item
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+        if let button = statusItem?.button {
+            button.image = NSImage(
+                systemSymbolName: "arrow.left.arrow.right",
+                accessibilityDescription: "Screen Transition Animator")
+            button.action = #selector(togglePopover)
+            button.target = self
+        }
+
+        // Create popover
+        popover = NSPopover()
+        popover?.contentSize = NSSize(width: 400, height: 550)
+        popover?.behavior = .transient
+        popover?.contentViewController = NSHostingController(
+            rootView: ContentView().environmentObject(screenMonitor)
+        )
+
+        // Auto-start monitoring
+        screenMonitor.startMonitoring()
+    }
+
+    @objc func togglePopover() {
+        if let button = statusItem?.button {
+            if popover?.isShown == true {
+                popover?.performClose(nil)
+            } else {
+                popover?.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            }
+        }
+    }
+}
